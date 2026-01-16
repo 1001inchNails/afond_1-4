3f4be887ae80f889b199382faf149fd277dc2729
1001inchNails <1001inchnails@gmail.com>
2026-01-16 19:10:19 +0100

    test06

--- api/app.js
+++ api/app.js
@@ -13,7 +13,7 @@ app.use(corsOptions);
 
 // rutas
 app.get("/", (req, res) => {
-  res.json({ message: "Escucha establecidaaaaaaahaaaaa" });
+  res.json({ message: "Escucha establecid" });
 });
 
 app.use(router);
